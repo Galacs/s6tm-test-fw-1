7cf050b6747c05ed53243df0bbdd5d4c83859311
Rémi Ait-Younes <remi@ait-younes.fr>
2026-05-19 22:08:36 +0200

    set custom board

--- main/play_mp3_control_example.c
+++ main/play_mp3_control_example.c
@@ -112,11 +112,7 @@ void app_main(void)
     audio_element_set_read_cb(mp3_decoder, mp3_music_read_cb, NULL);
 
     ESP_LOGI(TAG, "[2.2] Create i2s stream to write data to codec chip");
-#if defined CONFIG_ESP32_C3_LYRA_V2_BOARD
-    i2s_stream_cfg_t i2s_cfg = I2S_STREAM_PDM_TX_CFG_DEFAULT();
-#else
     i2s_stream_cfg_t i2s_cfg = I2S_STREAM_CFG_DEFAULT();
-#endif
     i2s_cfg.type = AUDIO_STREAM_WRITER;
     i2s_stream_writer = i2s_stream_init(&i2s_cfg);
 
@@ -170,66 +166,66 @@ void app_main(void)
             continue;
         }
 
-        if ((msg.source_type == PERIPH_ID_TOUCH || msg.source_type == PERIPH_ID_BUTTON || msg.source_type == PERIPH_ID_ADC_BTN)
-            && (msg.cmd == PERIPH_TOUCH_TAP || msg.cmd == PERIPH_BUTTON_PRESSED || msg.cmd == PERIPH_ADC_BUTTON_PRESSED)) {
-            if ((int) msg.data == get_input_play_id()) {
-                ESP_LOGI(TAG, "[ * ] [Play] touch tap event");
-                audio_element_state_t el_state = audio_element_get_state(i2s_stream_writer);
-                switch (el_state) {
-                    case AEL_STATE_INIT :
-                        ESP_LOGI(TAG, "[ * ] Starting audio pipeline");
-                        audio_pipeline_run(pipeline);
-                        break;
-                    case AEL_STATE_RUNNING :
-                        ESP_LOGI(TAG, "[ * ] Pausing audio pipeline");
-                        audio_pipeline_pause(pipeline);
-                        break;
-                    case AEL_STATE_PAUSED :
-                        ESP_LOGI(TAG, "[ * ] Resuming audio pipeline");
-                        audio_pipeline_resume(pipeline);
-                        break;
-                    case AEL_STATE_FINISHED :
-                        ESP_LOGI(TAG, "[ * ] Rewinding audio pipeline");
-                        audio_pipeline_reset_ringbuffer(pipeline);
-                        audio_pipeline_reset_elements(pipeline);
-                        audio_pipeline_change_state(pipeline, AEL_STATE_INIT);
-                        set_next_file_marker();
-                        audio_pipeline_run(pipeline);
-                        break;
-                    default :
-                        ESP_LOGI(TAG, "[ * ] Not supported state %d", el_state);
-                }
-            } else if ((int) msg.data == get_input_set_id()) {
-                ESP_LOGI(TAG, "[ * ] [Set] touch tap event");
-                ESP_LOGI(TAG, "[ * ] Stopping audio pipeline");
-                break;
-            } else if ((int) msg.data == get_input_mode_id()) {
-                ESP_LOGI(TAG, "[ * ] [mode] tap event");
-                audio_pipeline_stop(pipeline);
-                audio_pipeline_wait_for_stop(pipeline);
-                audio_pipeline_terminate(pipeline);
-                audio_pipeline_reset_ringbuffer(pipeline);
-                audio_pipeline_reset_elements(pipeline);
-                set_next_file_marker();
-                audio_pipeline_run(pipeline);
-            } else if ((int) msg.data == get_input_volup_id()) {
-                ESP_LOGI(TAG, "[ * ] [Vol+] touch tap event");
-                player_volume += 10;
-                if (player_volume > 100) {
-                    player_volume = 100;
-                }
-                audio_hal_set_volume(board_handle->audio_hal, player_volume);
-                ESP_LOGI(TAG, "[ * ] Volume set to %d %%", player_volume);
-            } else if ((int) msg.data == get_input_voldown_id()) {
-                ESP_LOGI(TAG, "[ * ] [Vol-] touch tap event");
-                player_volume -= 10;
-                if (player_volume < 0) {
-                    player_volume = 0;
-                }
-                audio_hal_set_volume(board_handle->audio_hal, player_volume);
-                ESP_LOGI(TAG, "[ * ] Volume set to %d %%", player_volume);
-            }
-        }
+        // if ((msg.source_type == PERIPH_ID_TOUCH || msg.source_type == PERIPH_ID_BUTTON || msg.source_type == PERIPH_ID_ADC_BTN)
+        //     && (msg.cmd == PERIPH_TOUCH_TAP || msg.cmd == PERIPH_BUTTON_PRESSED || msg.cmd == PERIPH_ADC_BUTTON_PRESSED)) {
+        //     if ((int) msg.data == get_input_play_id()) {
+        //         ESP_LOGI(TAG, "[ * ] [Play] touch tap event");
+        //         audio_element_state_t el_state = audio_element_get_state(i2s_stream_writer);
+        //         switch (el_state) {
+        //             case AEL_STATE_INIT :
+        //                 ESP_LOGI(TAG, "[ * ] Starting audio pipeline");
+        //                 audio_pipeline_run(pipeline);
+        //                 break;
+        //             case AEL_STATE_RUNNING :
+        //                 ESP_LOGI(TAG, "[ * ] Pausing audio pipeline");
+        //                 audio_pipeline_pause(pipeline);
+        //                 break;
+        //             case AEL_STATE_PAUSED :
+        //                 ESP_LOGI(TAG, "[ * ] Resuming audio pipeline");
+        //                 audio_pipeline_resume(pipeline);
+        //                 break;
+        //             case AEL_STATE_FINISHED :
+        //                 ESP_LOGI(TAG, "[ * ] Rewinding audio pipeline");
+        //                 audio_pipeline_reset_ringbuffer(pipeline);
+        //                 audio_pipeline_reset_elements(pipeline);
+        //                 audio_pipeline_change_state(pipeline, AEL_STATE_INIT);
+        //                 set_next_file_marker();
+        //                 audio_pipeline_run(pipeline);
+        //                 break;
+        //             default :
+        //                 ESP_LOGI(TAG, "[ * ] Not supported state %d", el_state);
+        //         }
+        //     } else if ((int) msg.data == get_input_set_id()) {
+        //         ESP_LOGI(TAG, "[ * ] [Set] touch tap event");
+        //         ESP_LOGI(TAG, "[ * ] Stopping audio pipeline");
+        //         break;
+        //     } else if ((int) msg.data == get_input_mode_id()) {
+        //         ESP_LOGI(TAG, "[ * ] [mode] tap event");
+        //         audio_pipeline_stop(pipeline);
+        //         audio_pipeline_wait_for_stop(pipeline);
+        //         audio_pipeline_terminate(pipeline);
+        //         audio_pipeline_reset_ringbuffer(pipeline);
+        //         audio_pipeline_reset_elements(pipeline);
+        //         set_next_file_marker();
+        //         audio_pipeline_run(pipeline);
+        //     } else if ((int) msg.data == get_input_volup_id()) {
+        //         ESP_LOGI(TAG, "[ * ] [Vol+] touch tap event");
+        //         player_volume += 10;
+        //         if (player_volume > 100) {
+        //             player_volume = 100;
+        //         }
+        //         audio_hal_set_volume(board_handle->audio_hal, player_volume);
+        //         ESP_LOGI(TAG, "[ * ] Volume set to %d %%", player_volume);
+        //     } else if ((int) msg.data == get_input_voldown_id()) {
+        //         ESP_LOGI(TAG, "[ * ] [Vol-] touch tap event");
+        //         player_volume -= 10;
+        //         if (player_volume < 0) {
+        //             player_volume = 0;
+        //         }
+        //         audio_hal_set_volume(board_handle->audio_hal, player_volume);
+        //         ESP_LOGI(TAG, "[ * ] Volume set to %d %%", player_volume);
+        //     }
+        // }
     }
 
     ESP_LOGI(TAG, "[ 6 ] Stop audio_pipeline");
